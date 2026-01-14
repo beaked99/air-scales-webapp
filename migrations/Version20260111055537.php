70255883535d24e0c71b6234fbf14cd24060f564
beaker <kwiebe0@gmail.com>
2026-01-13 23:00:11 -0800

    test

---
+++ migrations/Version20260111055537.php
@@ -0,0 +1,19 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20260111055537 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Placeholder: migration executed on this DB but file was not committed.';
+    }
+
+    public function up(Schema $schema): void {}
+    public function down(Schema $schema): void {}
+}
