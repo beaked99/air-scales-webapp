4d393b953e8e1dd380f10ad0f9818796ae4b49e5
beaker <kwiebe0@gmail.com>
2026-01-15 23:31:32 -0800

    working on the app section now

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -12,9 +12,13 @@ class SecurityController extends AbstractController
     #[Route(path: '/login', name: 'app_login')]
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
-        // if ($this->getUser()) {
-        //     return $this->redirectToRoute('target_path');
-        // }
+        if ($this->getUser()) {
+            // Redirect based on role
+            if (in_array('ROLE_ADMIN', $this->getUser()->getRoles(), true)) {
+                return $this->redirectToRoute('admin');
+            }
+            return $this->redirectToRoute('app_dashboard');
+        }
 
         // get the login error if there is one
         $error = $authenticationUtils->getLastAuthenticationError();
